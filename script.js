8f2971c44162b418f059449ed4e1a4a6e9ac9375
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-29 16:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //your JS code here. If required.
-const divElement=document.getElementById("output")
+function callme(){
+	const divElement=document.getElementById("output")
 divElement.innerHTML="Hello, world!"
+}
+window.setTimeout(callme,1000)
