d291d5d227a76d99d999c13694d9d03ee08d215a
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-07-06 11:42:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-//your JS code here. If required.
-// function callme(){
-// 	const divElement=document.getElementById("output")
-// divElement.innerHTML="Hello, world!"
-// }
-// window.setTimeout(callme,1000)
-
 function generatePromise(){
 	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-				resolve("Hello, world!")
-	
-		},1000)
+        setTimeout(()=>{
+            resolve("Hello, world!")
+        })
+    })
 	
 }
 generatePromise().then((data)=>{
 const divElement=document.getElementById("output")
 divElement.innerText=data;
 })
-
